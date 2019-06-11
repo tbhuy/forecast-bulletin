c470c1351326e19aa06c71bcbe51ec12a7ebb506
Huy Tran <tranbahuy@gmail.com>
2019-06-11 11:35:58 +0200

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
+#!/bin/bash
 WORKDIR /sparkinclimate
-
-RUN ls -ls
 # Download and process PDF dataset
 #RUN python3 bin/wait --block 300
 #RUN curl -XPUT http://localhost:9200/sparkinclimate -d @data/index_mapping.json
